ff6d17312d56458b259d73760369d80a220ba639
hackaholic <juniourkawasakicool@gmail.com>
2025-04-02 09:44:02 +0530

    Prototype Pattern

--- Creational_Pattern/Prototype/prototype1.py
+++ Creational_Pattern/Prototype/prototype1.py
@@ -21,3 +21,12 @@ cof2.size = "Medium"
 
 print(cof1)
 print(cof2)
+
+"""
+Prototype Pattern and SOLID Principles
+Single Responsibility Principle (SRP)	✅ The cloning logic is separated from the object's business logic.
+Open-Closed Principle (OCP)	            ✅ Extending the class does not require modifying existing code.
+Liskov Substitution Principle (LSP)	    ✅ Cloned objects should behave the same as originals, ensuring LSP compliance.
+Interface Segregation Principle (ISP)	✅ No unnecessary methods are forced on subclasses.
+Dependency Inversion Principle (DIP)	✅ Cloning works with abstractions rather than direct dependencies."
+"""
